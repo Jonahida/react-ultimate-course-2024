f1a2e11b817b38a82cc65e4bcf0cf7bc527a5339
jonahida <jonathan.hidalgo85@gmail.com>
2024-06-25 23:40:55 +0200

    Completing part-02-intermediate -> Section 10 (included)

---
+++ part-02-intermediate/07-usepopcorn/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StartRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color="blue" maxRating="10" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} starts </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okey", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
+  </React.StrictMode>
+);
